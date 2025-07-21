c1e46a118d7a4772f1ee57d87c830f9e40a796d8
Emarnav <emarnav@gmail.com>
2025-07-21 23:50:33 +0200

    next config production

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,12 +5,17 @@ const nextConfig: NextConfig = {
     buildActivity: false,
   },
   images: {
-    domains: ["localhost"],
+    domains: ["localhost", "alojamientos-lemon.vercel.app"], // añade aquí tu dominio real
     remotePatterns: [
       {
         protocol: "http",
         hostname: "localhost",
         port: "3001",
+        pathname: "/alojamientos/**",
+      },
+      {
+        protocol: "https",
+        hostname: "alojamientos-lemon.vercel.app", // ejemplo de dominio real
         pathname: "/alojamientos/**",
       },
     ],
